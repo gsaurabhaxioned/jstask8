0aacfefe0d6245b93016ffd0d7eec18f8f4995e4
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-14 19:14:01 +0530

    js in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
-const inputs = document.querySelectorAll('.user-input');
-
+const inputs = document.querySelectorAll('.user-input'),
+ labels = document.querySelectorAll('.input-label');
+console.log(labels);
 for(i=0;i<inputs.length;i++){
     inputs[i].addEventListener('click', function(){
-        this.classList.toggle('input-focus');
-    },true);
+
+        labels[i].classList.toggle('input-focus');
+    });
 }
 
 
