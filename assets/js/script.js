68cec9f16aeb4e928bba13ddab627cee6ad6aeeb
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-14 20:17:38 +0530

    js code for toggling done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,9 @@
 const inputs = document.querySelectorAll('.user-input'),
- labels = document.querySelectorAll('.input-label');
-console.log(labels);
-for(i=0;i<inputs.length;i++){
-    inputs[i].addEventListener('click', function(){
+    labels = document.querySelectorAll('.input-label');
 
+for (let i = 0; i < inputs.length; i++) {
+
+    inputs[i].addEventListener('click', function () {
         labels[i].classList.toggle('input-focus');
     });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
