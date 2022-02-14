2f5d72be7f9ff377d18990a632f7c8a5d1fe14bd
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-14 16:22:45 +0530

    js in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
-/* Author: 
+const user_input = document.querySelectorAll('.user-input');
 
-*/
-
-
+for(let i=0;i<user_input.length;i++){
+    user_input[i].addEventListener('focus',function(){
+        this.style.marginTop= "10px";
+    },true);
+}
 
 
 
