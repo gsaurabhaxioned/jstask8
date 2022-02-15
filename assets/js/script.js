81b0b9f13c5273f7dfd425ff86e4887a183f78f8
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-15 15:26:59 +0530

    js code for filter and blur added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 let email_field = document.querySelector('input[name="email"]'),
     email = document.querySelector('#email'),
-    error_msg = document.querySelectorAll('.error'),
+    label = document.querySelectorAll('.input-label'),
+    input_box = document.querySelectorAll('.input-box');
+error_msg = document.querySelectorAll('.error'),
     error = document.querySelector('#error'),
     email_pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
@@ -12,3 +14,20 @@ email.addEventListener('keypress', function () {
         error.innerHTML = "please enter valid email";
     }
 });
+
+
+for (let i = 0; i < input_box.length; i++) {
+    input_box[i].addEventListener('focus', function () {
+        for (let j = 0; j < label.length; j++) {
+            label[j].classList.add('movespan');
+        }
+    });
+
+
+    input_box[i].addEventListener('blur', function () {
+        for (let j = 0; j < label.length; j++) {
+            label[j].classList.remove('movespan');
+            label[j].classList.add('movespandown');
+        }
+    });
+}
