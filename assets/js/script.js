22c9c6a54fb70dc1b6b1fd75604b87f5af4756cf
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-15 11:05:25 +0530

    css styling updated for transition effects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
-const email_value = document.querySelector('#email').value,
+let email_value = document.querySelector('input[name="email"]'),
     email = document.querySelector('#email'),
-    password = document.querySelector('#password').value,
     error_msg = document.querySelectorAll('.error'),
     error = document.querySelector('#error'),
 email_pattern =  /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+console.log(email_value);
 email.addEventListener('keypress', function () {
-    if (email_value.match(email_pattern)) {
+    console.log(email_value.value);
+    if (email_value.value.match(email_pattern)) {
         error.innerHTML = "";
     } else {
         error.innerHTML = "please enter valid email";
