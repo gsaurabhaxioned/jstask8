02b54575975f811a547925e651379b9eca6fb131
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-15 10:23:47 +0530

    js code for email validation in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
-const email_value =document.getElementById('email').value,
-email = document.getElementById('email'),
-password = document.getElementById('password').value,
-error_msg = document.querySelectorAll('.error'),
-email_pattern= document.querySelector('error');
-email.addEventListener('keypress', function(){
-    if(!email_value.match(email_pattern)){
-        error.innerHTML="please enter valid email";
-    } 
+const email_value = document.querySelector('#email').value,
+    email = document.querySelector('#email'),
+    password = document.querySelector('#password').value,
+    error_msg = document.querySelectorAll('.error'),
+    error = document.querySelector('#error'),
+email_pattern =  /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+email.addEventListener('keypress', function () {
+    if (email_value.match(email_pattern)) {
+        error.innerHTML = "";
+    } else {
+        error.innerHTML = "please enter valid email";
+    }
 });
