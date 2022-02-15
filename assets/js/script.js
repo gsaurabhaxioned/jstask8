435bbd53cfd02e19ca11af36349aa9c5db0c9bc6
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-15 11:15:11 +0530

    js code completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,11 @@ let email_field = document.querySelector('input[name="email"]'),
     email = document.querySelector('#email'),
     error_msg = document.querySelectorAll('.error'),
     error = document.querySelector('#error'),
-email_pattern =  /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    email_pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
 email.addEventListener('keypress', function () {
-    let email_value=email_field.value;
-    if (email_value.value.match(email_pattern)) {
+    let email_value = email_field.value;
+    if (email_value.match(email_pattern)) {
         error.innerHTML = "";
     } else {
         error.innerHTML = "please enter valid email";
